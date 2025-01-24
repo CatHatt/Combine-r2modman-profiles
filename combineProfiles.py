d3b54c087b29dbc4e807bb4d81d1405d8c65ca2a
CatHat <liase810@gmail.com>
2025-01-24 01:14:18 +0100

    fixed bug, added features and compiled to exe

--- combineProfiles.py
+++ combineProfiles.py
@@ -13,38 +13,45 @@ import yaml
 
 
 def noProfiles():
-    print('There are no profiles in the folder where the exe file is located. Look at README.txt to see if you did something wrong.')
-    input('Press enter to close...')
+    print(
+        'There are no profiles in the folder where the exe file is located. Look at README.txt to see if you did something wrong.'
+    )
     close()
 
+
 def close():
-    print('Closing in...')
-    sleep(1)
-    for i in range(0, 3):
-        print(str(3-i) + '...')
-        sleep(1)
+    input('Press enter to close...')
+    # print('Closing in...')
+    # sleep(1)
+    # for i in range(0, 3):
+    #    print(str(3 - i) + '...')
+    #    sleep(1)
     print('Goodbye :)')
     sys.exit()
+
 
 def separatorLine():
     print('-' * 70)
 
-allProfiles = [f for f in os.listdir() if os.path.isdir(f) and os.path.exists(f + '/mods.yml')]
+
+allProfiles = [
+    f for f in os.listdir() if os.path.isdir(f) and os.path.exists(f + '/mods.yml')
+]
 
 if allProfiles == []:
     noProfiles()
 
 profiles = inquirer.checkbox(
-    message = 'What profiles do you want to combine?\n(Space: Toggle, Enter: Submit)',
-    choices = allProfiles,
-    validate = lambda result: len(result) > 1,
-    invalid_message = 'Make 2 or more selections',
+    message='What profiles do you want to combine?\n(Space: Toggle, Enter: Submit)',
+    choices=allProfiles,
+    validate=lambda result: len(result) > 1,
+    invalid_message='Make 2 or more selections',
 ).execute()
 
 outputName = inquirer.text(
-    message = 'What do you want the output profile to be named?\n(Enter: Submit)\n',
-    validate = lambda result: len(result) != 0 and re.match('^[A-Za-z0-9. ]*$', result),
-    invalid_message = 'Name must adhere to rules: Must contain at least 1 character | Can only contain English letters, numbers, spaces and dots'
+    message='What do you want the output profile to be named?\n(Enter: Submit)\n',
+    validate=lambda result: len(result) != 0 and re.match('^[A-Za-z0-9. ]*$', result),
+    invalid_message='Name must adhere to rules: Must contain at least 1 character | Can only contain English letters, numbers, spaces and dots',
 ).execute(separatorLine())
 
 if outputName[0] == '.' or outputName[0] == ' ':
@@ -55,7 +62,9 @@ if outputName[-1] == '.' or outputName[-1] == ' ':
 print(outputName)
 
 if os.path.exists(outputName):
-    if not inquirer.confirm(message = 'This profile already exists! Are you sure you want to overwrite it?').execute(separatorLine()):
+    if not inquirer.confirm(
+        message='This profile already exists! Are you sure you want to overwrite it?'
+    ).execute(separatorLine()):
         print('Canceled...')
         close()
 
@@ -69,9 +78,13 @@ allYamlMods = []
 allModNames = []
 
 for profile in profiles:
-    with open(profile + '/mods.yml', 'r') as file:
+    with open(f'{profile}/mods.yml', 'r') as file:
         yamlMods = yaml.safe_load(file)
-        modNames = [d['name'] for d in yamlMods]
+        try:
+            modNames = [d['name'] for d in yamlMods]
+        except:
+            print(f'! Profile "{profile}" contains no mods, continuing...')
+            continue
 
         if allModNames != []:
             for name in modNames:
@@ -79,13 +92,21 @@ for profile in profiles:
                     i = modNames.index(name)
                     modNames.pop(i)
                     yamlMods.pop(i)
-        
-        allYamlMods += yamlMods
-        allModNames += modNames                
 
-with open(outputName + '/mods.yml', 'w') as outputFile:
+        allYamlMods += yamlMods
+        allModNames += modNames
+
+with open(f'{outputName}/mods.yml', 'w') as outputFile:
     yaml.dump(allYamlMods, outputFile)
 
-print('Finished!')
+print('Finished!\n')
+
+print(
+    f'"{outputName}" profile contains the following mods:\n{'\n    '.join(allModNames)}\n'
+)
+
+print('! Make sure to reinstall mods! (Refer to README)')
+
+separatorLine()
 
 close()
